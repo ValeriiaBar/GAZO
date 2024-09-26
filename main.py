07331a7629ca19cce64cb4b2a13dd85a14b989db
jillllek <154076863+jillllek@users.noreply.github.com>
2024-09-26 20:31:13 +0300

    добавлены модули с константами и расчётом скоростей превращения веществ

---
+++ main.py
@@ -0,0 +1,58 @@
+# import numpy as np
+# from constants import compounds_dict
+# R = 1.987
+#
+#
+# compound = 'Оксид углеродa'
+# t = 200 # ЦЕЛЬСИЙ, НЕ КЕЛЬВИНОВ
+# c = 3 # проц объёма
+# tau = 3 # с
+#
+#
+# constants = compounds_dict['Оксид углеродa']
+
+# sfgwjsfiwejroijweoijrg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
